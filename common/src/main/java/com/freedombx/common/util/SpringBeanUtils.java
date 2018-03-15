5bc9c8bf97b8ff9ec976d71ed042819a4eadfdd2
maboxiao <maboxiao@rongcapital.cn>
2018-03-15 16:55:23 +0800

    初始化SpringBoot，包括启动服务，日志配置

---
+++ common/src/main/java/com/freedombx/common/util/SpringBeanUtils.java
@@ -0,0 +1,27 @@
+package com.freedombx.common.util;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.util.ReflectionUtils;
+
+public class SpringBeanUtils {
+    private static ApplicationContext applicationContext = null;
+
+    public static void setApplicationContext(ApplicationContext applicationContext) {
+        SpringBeanUtils.applicationContext = applicationContext;
+    }
+
+    public static <T> T getBean(Class<T> type) {
+        return applicationContext.getBean(type);
+    }
+    /**
+     * 获取对象
+     *
+     * @param id
+     * @return Object 一个以所给名字注册的bean的实例
+     * @throws BeansException
+     */
+    public static Object getBean(String id) {
+        return applicationContext.getBean(id);
+    }
+}
